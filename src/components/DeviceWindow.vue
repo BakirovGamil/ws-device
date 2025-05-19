31a1338720266488ccd2329e8fed67b5957490a6
gamil <gamilbakirov@gmail.com>
2025-05-19 20:36:25 +0300

    add iframe bg

--- src/components/DeviceWindow.vue
+++ src/components/DeviceWindow.vue
@@ -47,7 +47,7 @@
         closable
         @close="isWarningOpen = false"
       />
-      <div class="w-full aspect-video flex items-center justify-center border-black border-8 rounded-lg">
+      <div class="w-full aspect-video flex items-center justify-center border-black  bg-neutral-200 border-8 rounded-lg">
         <div class="w-full aspect-video overflow-hidden relative">
           <iframe ref="frameRef" :key="address" class="absolute inset-0 w-full h-full" :src="`http://${address}`" />
         </div>
